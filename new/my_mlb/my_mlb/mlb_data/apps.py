884a770d2bfdfc248634e19c6c64ab524ff6a1de
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2025-04-23 17:56:49 -0400

    moved the files back

--- new/my_mlb/my_mlb/mlb_data/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MlbDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mlb_data'
--- new/my_mlb/settings.py
+++
@@ -1,8 +0,0 @@
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        # ... other settings ...
-    },
-] 
